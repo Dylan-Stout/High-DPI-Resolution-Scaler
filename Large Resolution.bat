f19917d437b8cb4fa686e723298c16cac946fb05
Dylan Stout <dylan.d.stout@gmail.com>
2014-12-31 04:53:55 -0700

    Resolution and dpi changer

---
+++ Large Resolution.bat
@@ -0,0 +1,3 @@
+regedit.exe /s C:\Tools\Resolution_Changer\190_DPI.reg
+C:\Tools\Resolution_Changer\QRes.exe /x:3840 /y:2160
+shutdown /r /f /t 00
---
+++ Small Resolution.bat
@@ -0,0 +1,3 @@
+regedit.exe /s C:\Tools\Resolution_Changer\96_DPI.reg
+C:\Tools\Resolution_Changer\QRes.exe /x:1920 /y:1080
+shutdown /r /f /t 00
